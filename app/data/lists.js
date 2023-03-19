6341e34ebd709b21af2cce7f515e3c46a8750f17
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-03-19 15:18:00 -0400

    fix: add rest of data

--- app/data/lists.js
+++ app/data/lists.js
@@ -7,20 +7,42 @@ export const LISTS = [
       {
         "id": "101",
         "name": "milk",
-        "checked": false,
-        "category": "dairy"
       },
       {
         "id": "102",
         "name": "cheese",
-        "checked": false,
-        "category": "dairy"
       },
       {
         "id": "103",
         "name": "oranges",
-        "checked": false,
-        "category": "fruit"
+      },
+      {
+        "id": "104",
+        "name": "chocolate milk",
+      },
+      {
+        "id": "105",
+        "name": "yogurt",
+      },
+      {
+        "id": "106",
+        "name": "apples",
+      },
+      {
+        "id": "107",
+        "name": "pizza",
+      },
+      {
+        "id": "108",
+        "name": "eggs",
+      },
+      {
+        "id": "109",
+        "name": "salad",
+      },
+      {
+        "id": "110",
+        "name": "cookies",
       }
     ]
   },
@@ -31,21 +53,411 @@ export const LISTS = [
       {
         "id": "201",
         "name": "tent",
-        "checked": false,
-        "category": "sporting goods"
       },
       {
         "id": "202",
-        "name": "chairs",
-        "checked": false,
-        "category": "sporting goods"
+        "name": "sleeping bag",
       },
       {
         "id": "203",
-        "name": " bug spray",
-        "checked": false,
-        "category": "insect repellent"
-      }
+        "name": "chairs",
+      },
+      {
+        "id": "204",
+        "name": " sunscreen",
+      },
+      {
+        "id": "205",
+        "name": "lantern",
+      },
+      {
+        "id": "206",
+        "name": "multi-tool",
+      },
+      {
+        "id": "207",
+        "name": "camp stove",
+      },
+      {
+        "id": "208",
+        "name": "matches",
+      },
+      {
+        "id": "209",
+        "name": "first-aid kit",
+      },
+      {
+        "id": "210",
+        "name": "large water jug",
+      },
     ]
-  }
+  },
+  {
+    "id": "3",
+    "name": "Games",
+    "listItems": [
+      {
+        "id": "301",
+        "name": "E.T. the Extra-Terrestrial",
+      },
+      {
+        "id": "302",
+        "name": "Donkey Kong",
+      },
+      {
+        "id": "303",
+        "name": "Pitfall!",
+      },
+      {
+        "id": "304",
+        "name": "Fast Food",
+      },
+      {
+        "id": "305",
+        "name": "Pac-Man",
+      },
+      {
+        "id": "306",
+        "name": "3D Pinball for Windows - Space Cadet",
+      },
+      {
+        "id": "307",
+        "name": "SimCity 3000",
+      },
+      {
+        "id": "308",
+        "name": "Microsoft Jigsaw",
+      },
+      {
+        "id": "309",
+        "name": "Taptiles",
+      },
+      {
+        "id": "310",
+        "name": "Microsoft Mahjong",
+      },
+    ]
+  },
+  {
+    "id": "4",
+    "name": "Dogs",
+    "listItems": [
+      {
+        "id": "401",
+        "name": "Timmy",
+      },
+      {
+        "id": "402",
+        "name": "Anna",
+      },
+      {
+        "id": "403",
+        "name": "Jack",
+      },
+      {
+        "id": "404",
+        "name": "Susie",
+      },
+      {
+        "id": "405",
+        "name": "Moses",
+      },
+      {
+        "id": "406",
+        "name": "Ice Cream",
+      },
+      {
+        "id": "407",
+        "name": "Thor",
+      },
+      {
+        "id": "408",
+        "name": "Chocolate",
+      },
+      {
+        "id": "409",
+        "name": "Ginger",
+      },
+      {
+        "id": "410",
+        "name": "Lassie",
+      },
+    ]
+  },
+  {
+    "id": "5",
+    "name": "Chores",
+    "listItems": [
+      {
+        "id": "501",
+        "name": "Wash dishes",
+      },
+      {
+        "id": "502",
+        "name": "Vacuum",
+      },
+      {
+        "id": "503",
+        "name": "Laundry",
+      },
+      {
+        "id": "504",
+        "name": "Mow lawn",
+      },
+      {
+        "id": "505",
+        "name": "Weed yard",
+      },
+      {
+        "id": "506",
+        "name": "Dust",
+      },
+      {
+        "id": "507",
+        "name": "Sweep kitchen",
+      },
+      {
+        "id": "508",
+        "name": "Mop kitchen",
+      },
+      {
+        "id": "509",
+        "name": "Clean bathroom",
+      },
+      {
+        "id": "510",
+        "name": "Take out trash",
+      },
+    ]
+  },
+  {
+    "id": "6",
+    "name": "Programming Languages",
+    "listItems": [
+      {
+        "id": "601",
+        "name": "Java",
+      },
+      {
+        "id": "602",
+        "name": "JavaScript",
+      },
+      {
+        "id": "603",
+        "name": "PHP",
+      },
+      {
+        "id": "604",
+        "name": "Rust",
+      },
+      {
+        "id": "605",
+        "name": "Python",
+      },
+      {
+        "id": "606",
+        "name": "Go",
+      },
+      {
+        "id": "607",
+        "name": "C#",
+      },
+      {
+        "id": "608",
+        "name": "Swift",
+      },
+      {
+        "id": "609",
+        "name": "HTML",
+      },
+      {
+        "id": "610",
+        "name": "R",
+      },
+    ]
+  },
+  {
+    "id": "7",
+    "name": "Web Frameworks",
+    "listItems": [
+      {
+        "id": "701",
+        "name": "React",
+      },
+      {
+        "id": "702",
+        "name": "Angular",
+      },
+      {
+        "id": "703",
+        "name": "Ruby on Rails",
+      },
+      {
+        "id": "704",
+        "name": "Django",
+      },
+      {
+        "id": "705",
+        "name": "Vue",
+      },
+      {
+        "id": "706",
+        "name": "Laravel",
+      },
+      {
+        "id": "707",
+        "name": "Express",
+      },
+      {
+        "id": "708",
+        "name": "ASP .NET",
+      },
+      {
+        "id": "709",
+        "name": "Spring",
+      },
+      {
+        "id": "710",
+        "name": "Code Igniter",
+      },
+    ]
+  },
+  {
+    "id": "8",
+    "name": "TV / Streaming Stations",
+    "listItems": [
+      {
+        "id": "801",
+        "name": "Magellan TV",
+      },
+      {
+        "id": "802",
+        "name": "DryBar Comedy",
+      },
+      {
+        "id": "803",
+        "name": "TVLand Drama",
+      },
+      {
+        "id": "804",
+        "name": "TVLand Sitcom",
+      },
+      {
+        "id": "805",
+        "name": "Samsung Wild Life",
+      },
+      {
+        "id": "806",
+        "name": "Quest",
+      },
+      {
+        "id": "807",
+        "name": "MeTV",
+      },
+      {
+        "id": "808",
+        "name": "Buzzr",
+      },
+      {
+        "id": "809",
+        "name": "Tiny House Nation",
+      },
+      {
+        "id": "810",
+        "name": "Bounce XL",
+      },
+    ]
+  },
+  {
+    "id": "9",
+    "name": "TV Shows",
+    "listItems": [
+      {
+        "id": "901",
+        "name": "Nova",
+      },
+      {
+        "id": "902",
+        "name": "Nature",
+      },
+      {
+        "id": "903",
+        "name": "This Old House",
+      },
+      {
+        "id": "904",
+        "name": "Tiny House Hunting",
+      },
+      {
+        "id": "905",
+        "name": "Classic Concentration",
+      },
+      {
+        "id": "906",
+        "name": "Jeopardy",
+      },
+      {
+        "id": "907",
+        "name": "Wheel of Fortune",
+      },
+      {
+        "id": "908",
+        "name": "In the Cut",
+      },
+      {
+        "id": "909",
+        "name": "Hot in Cleveland",
+      },
+      {
+        "id": "910",
+        "name": "Family Feud",
+      },
+    ]
+  },
+  {
+    "id": "10",
+    "name": "Mock Data Tools",
+    "listItems": [
+      {
+        "id": "1001",
+        "name": "Faker",
+      },
+      {
+        "id": "1002",
+        "name": "Mocker Data Generator",
+      },
+      {
+        "id": "1003",
+        "name": "JSON Placeholder",
+      },
+      {
+        "id": "1004",
+        "name": "Generate Data",
+      },
+      {
+        "id": "1005",
+        "name": "JSON Schema Faker",
+      },
+      {
+        "id": "1006",
+        "name": "FakeStoreAPI",
+      },
+      {
+        "id": "1007",
+        "name": "Mock Turtle",
+      },
+      {
+        "id": "1008",
+        "name": "Random User",
+      },
+      {
+        "id": "1009",
+        "name": "Fake Logos",
+      },
+      {
+        "id": "1010",
+        "name": "Mockaroo",
+      },
+    ]
+  },
 ];
